8dcb61247eb689209289d12e787796f0d8a3a1ad
nguyenminhquan2204 <142667586+nguyenminhquan2204@users.noreply.github.com>
2024-05-24 02:10:30 +0700

    Lấy ra danh sách bài viết

--- resolvers.ts
+++ resolvers.ts
@@ -1,8 +1,17 @@
+import Article from "./models/article.model";
 
 export const resolvers = {
     Query: {
         hello: () => {
             return "Quan Chi";
+        },
+
+        getListArticle: async () => {
+            const articles = await Article.find({
+                deleted: false
+            });
+
+            return articles;
         }
     }
 };
